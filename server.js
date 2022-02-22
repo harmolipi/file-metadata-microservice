ec13effe7950cf5a66dd1a67808e67d361224e8e
harmolipi <nikob381@gmail.com>
2022-02-22 14:47:02 -0500

    Imported multer

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
+require('dotenv').config();
+const upload = multer({ dest: 'uploads/' });
 
 var app = express();
 
@@ -8,13 +9,10 @@ app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+  res.sendFile(process.cwd() + '/views/index.html');
 });
-
-
-
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log('Your app is listening on port ' + port);
 });
