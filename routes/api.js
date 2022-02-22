3231a8e6977e6802f939dd31962120029587fbe1
harmolipi <nikob381@gmail.com>
2022-02-22 15:03:14 -0500

    Moved multer to route and added upload attribute

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
+
 const api_controller = require('../controllers/apiController');
 
-router.post('/fileanalyze', api_controller.fileAnalyze);
+router.post(
+  '/fileanalyse',
+  upload.single('upfile'),
+  api_controller.fileAnalyse
+);
 
 module.exports = router;
